ca4b86a7fd2bd745be7bba4ae03b26c1c2b503f8
rosario-lombardo <rosario.lombardo@gmail.com>
2020-11-12 14:35:41 +0100

    run training before conflicts

--- run_training.sh
+++ run_training.sh
@@ -1,47 +1,66 @@
 #!/bin/bash
 
-# From beesl project home type:
-# ./run_training.sh "EXPERIMENT_NAME" config/base-genia.json config/cosbi-biobert-base-params.json
-
-#param $1 = experiment name, same name will be placed on the logs/ folder
-#param $2 = configuration file for data for training
-#param $3 = configuration file for the bert model and network parameters
-
-if [ $# -ne 3 ]; then
-    echo "Exactly 3 args are required: arg1=name, arg2=training data json path, arg3=model json path"
-    echo "Multiple training data and models can be given separated by ':', exp name will be auto-renamed"
-    echo "Ex1 ./run_training.sh 'EXPERIMENT_NAME' config/base-genia.json config/cosbi-biobert-base-params.json"
-    echo "Ex2 ./run_training.sh 'EXP' conf/genia.json:conf/epi.json conf/cosbi-bert.json:conf/MSbert.json:conf/scibert.json"
-    echo "REMEMBER: Activate environment with 'conda activate beesl-env'"
-
+if [ $# -ne 4 ]; then
+    echo
+    echo "Execute from beesl project home. Exactly 4 args are required:"
+    echo "  arg1 = model JSON path"
+    echo "  arg2 = training data JSON path"
+    echo "  arg3 = validation FOLDER path"
+    echo "  arg4 = testing TOKEN FILE path"
+    echo
+    echo "Eg1 ./run_training.sh config/cosbi-biobert-base-params.json              \\"
+    echo "                      config/base-genia.json                             \\"
+    echo "                      data/corpora/GE11/dev                              \\"
+    echo "                      data/GE11/not-masked/dev.mt.1"
+    echo 
+    echo "Multiple models and training datasets can be given separated by ':'"
+    echo
+    echo "Eg2 ./run_training.sh conf/bert.json:conf/MSbert.json:conf/scibert.json  \\"
+    echo "                      conf/genia.json:conf/epi.json                      \\"
+    echo "                      data/corpora/GE11/dev                              \\"
+    echo "                      data/GE11/not-masked/dev.mt.1"
+    echo
+    echo "Experiment names will be auto-named as 'model_training_testing'"
+    echo "REMEMBER: Activate ENV with 'conda activate beesl-env' prior to execution"
+    echo
     exit 1
 fi
 
+IFS=':' read -r -a models <<< "$1"
 IFS=':' read -r -a traindata <<< "$2"
-IFS=':' read -r -a models <<< "$3"
-    
-for train in "${traindata[@]}"; do
-	trainfile="${train##*/}"
-	trainfile="${trainfile%%.*}"
+validationdir="$3"
+testfile="${4##*/}"
+
+for model in "${models[@]}"; do
+	modelfile="${model##*/}"
+	modelfile="${modelfile%%.*}"
 	
-	for model in "${models[@]}"; do
-		modelfile="${model##*/}"
-		modelfile="${modelfile%%.*}"
-		
-		name="${1}_${trainfile}_${modelfile}"
+	for train in "${traindata[@]}"; do
+		trainfile="${train##*/}"
+		trainfile="${trainfile%%.*}"
+	
+		name="${modelfile}_${trainfile}_${testfile}"
+
 		# Run training
 		python train.py --name ${name} --dataset_config ${train} --parameters_config ${model} --device 0
 
-		tstamp=`ls ~/beesl/logs/$1 | sort -k 2 | tail -1`
-		mv ~/beesl/logs/${1}/${tstamp}/results.txt ~/beesl/logs/${1}/${tstamp}/results_${name}_${tstamp}.txt
-		mv ~/beesl/logs/${1}/${tstamp}/metrics.txt ~/beesl/logs/${1}/${tstamp}/metrics_${name}_${tstamp}.txt
+		tstamp=`ls ~/beesl/logs/${name} | sort -k 2 | tail -1`
+		
+		# Deflatten and evaluate
+		python predict.py logs/${name}/${tstamp}/model.tar.gz ${testfile} logs/${name}/${tstamp}/pred.txt --device 0
+		python bio-mergeBack.py logs/${name}/${tstamp}/pred.txt ${testfile} 2 > logs/${name}/${tstamp}/pred-notmasked.txt
+		python bioscripts/postprocess.py --filepath logs/${name}/${tstamp}/pred-notmasked.txt && mv output logs/${name}/${tstamp}/
+		perl bioscripts/eval/a2-normalize.pl -v -g ${validationdir} -o logs/${name}/${tstamp}/output_norm logs/${name}/${tstamp}/output/*.a2
+		perl bioscripts/eval/a2-evaluate.pl -g ${validationdir} -t1 -sp logs/${name}/${tstamp}/output_norm/*.a2 > logs/${name}/${tstamp}/results.txt
+		
+		mv ~/beesl/logs/${name}/${tstamp}/results.txt ~/beesl/logs/${name}/${tstamp}/results_${name}_${tstamp}.txt
+		mv ~/beesl/logs/${name}/${tstamp}/metrics.txt ~/beesl/logs/${name}/${tstamp}/metrics_${name}_${tstamp}.txt
 
 		# Copy data where needed, eg, an AWS bucket
-		aws s3 cp ~/beesl/logs/${1}/${tstamp}/results_${name}_${tstamp}.txt s3://bucket
-		aws s3 cp ~/beesl/logs/${1}/${tstamp}/metrics_${name}_${tstamp}.txt s3://bucket
+		aws s3 cp ~/beesl/logs/${name}/${tstamp}/results_${name}_${tstamp}.txt s3://cosbi-beesl
+		aws s3 cp ~/beesl/logs/${name}/${tstamp}/metrics_${name}_${tstamp}.txt s3://cosbi-beesl
 	done
 done
    
 # Post training actions, eg, containers, shutdown, etc.
-sudo halt 
-    
+#sudo halt 
